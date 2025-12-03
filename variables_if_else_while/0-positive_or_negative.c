a71bb492a59b88155927adbb040edab2f8f35569
MalikHolber <kamalatluxanov13@gmail.com>
2025-12-03 08:10:20 +0000

    a

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -21,15 +21,15 @@ srand(time(NULL));
 a = rand() - RAND_MAX / 2;
 
 if(a>0){
-printf("%d is positive", a);
+printf("%d is positive\n", a);
 }
 
-else if(a == 0){
-printf("%d is a zero",a);
+else if(a < 0){
+printf("%d is negative\n",a);
 }
 
 else
-printf("%d is a negative", a);
+printf("%d is zero\n", a);
 
 return(0);
 
