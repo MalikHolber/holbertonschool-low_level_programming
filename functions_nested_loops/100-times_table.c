b28569bec5ba2f94fb03e7a752af15ea412555aa
MalikHolber <kamalatluxanov13@gmail.com>
2025-12-07 18:40:24 +0000

    a

--- functions_nested_loops/100-times_table.c
+++ functions_nested_loops/100-times_table.c
@@ -1,49 +1,114 @@
-/*
- * File: 100-times_table.c
- * Auth: Brennan D Baraban
- */
-
 #include "main.h"
 
 /**
- * print_times_table - Prints the times table of the input,
- *                     starting with 0.
- * @n: The value of the times table to be printed.
+ * print_thousand - print out the digit in the thousand spot
+ * @sum: Value to scan for thousand spot
+ *
+ * Return: Void
+ */
+static void print_thousand(int sum)
+{
+	if (sum / 1000 == 0)
+	{
+		_putchar(32);
+	}
+	else
+	{
+		_putchar('0' + (sum / 1000));
+	}
+}
+
+
+/**
+ * print_hundred - print out the digit in the hundred spot
+ * @sum: Value to scan for hundred spot
+ *
+ * Return: Void
+ */
+static void print_hundred(int sum)
+{
+	if (sum / 100 == 0)
+	{
+		_putchar(32);
+	}
+	else
+	{
+		_putchar('0' + (sum / 100));
+	}
+}
+
+
+/**
+ * print_tenth - print out the digit in the tenth spot
+ * @sum: Value to scan for tenth spot
+ *
+ * Return: Void
+ */
+static void print_tenth(int sum)
+{
+	if (sum / 10 == 0)
+	{
+		_putchar(32);
+	}
+	else
+	{
+		if ((sum / 10) > 9)
+		{
+			_putchar('0' + (sum / 10 % 10));
+		}
+		else
+		{
+			_putchar('0' + (sum / 10));
+		}
+	}
+}
+
+
+/**
+ * print_base10 - print out the digit in the base10 spot
+ * @sum: Value to scan for base10 spot
+ *
+ * Return: Void
+ */
+static void print_base10(int sum)
+{
+	if (sum % 10 == 0)
+	{
+		_putchar('0');
+	}
+	else
+	{
+		_putchar('0' + (sum % 10));
+	}
+}
+
+/**
+ * print_times_table - print out the 9 times table
+ * @n: Valuse use to generate times tables.
+ *
+ * Return: void
  */
 void print_times_table(int n)
 {
-	int num, mult, prod;
+	int v, h;
 
-	if (n >= 0 && n <= 15)
+	if (n > 15)
 	{
-		for (num = 0; num <= n; num++)
+		return;
+	}
+	for (h = 0 ; h < n + 1 ; h++)
+	{
+		_putchar('0');
+		for (v = 1 ; v < n + 1 ; v++)
 		{
-			_putchar('0');
+			int sum = v * h;
 
-			for (mult = 1; mult <= n; mult++)
-			{
-				_putchar(',');
-				_putchar(' ');
-
-				prod = num * mult;
-
-				if (prod <= 99)
-					_putchar(' ');
-				if (prod <= 9)
-					_putchar(' ');
-
-				if (prod >= 100)
-				{
-					_putchar((prod / 100) + '0');
-					_putchar(((prod / 10)) % 10 + '0');
-				}
-				else if (prod <= 99 && prod >= 10)
-				{
-					_putchar((prod / 10) + '0');
-				}
-				_putchar((prod % 10) + '0');
-			}
-			_putchar('\n');
+			_putchar(',');
+			print_thousand(sum);
+			print_hundred(sum);
+			print_tenth(sum);
+			print_base10(sum);
 		}
+		_putchar('\n');
 	}
 }
