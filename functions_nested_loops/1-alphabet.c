cffad31ee9963c2bedf6d57a9de1efe27595cbca
MalikHolber <kamalatluxanov13@gmail.com>
2025-12-07 17:20:00 +0000

    a

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,10 +1,19 @@
-#include "main.h"
+/*
+ * File: 1-alphabet.c
+ * Auth: Brennan D Baraban
+ */
 
+#include "holberton.h"
+
+/**
+ * print_alphabet - prints the alphabet in lowercase, followed by a new line.
+ */
 void print_alphabet(void)
 {
-    char a;
+	char letter;
 
-    for (a = 'a'; a <= 'z'; a++)
-        _putchar(a);
-    _putchar('\n');
+	for (letter = 'a'; letter <= 'z'; letter++)
+		_putchar(letter);
+
+	_putchar('\n');
 }
