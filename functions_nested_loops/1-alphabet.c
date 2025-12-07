9ed83d229da970037d02b7b5579d19481960d199
MalikHolber <kamalatluxanov13@gmail.com>
2025-12-07 17:19:19 +0000

    a

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,12 +1,10 @@
-
 #include "main.h"
 
-void print_alphabet(void){
+void print_alphabet(void)
+{
+    char a;
 
-char a = 'a';
-
-for(a = 'a'; a<='z'; a++){
-    _putchar(a);
-}
-
+    for (a = 'a'; a <= 'z'; a++)
+        _putchar(a);
+    _putchar('\n');
 }
