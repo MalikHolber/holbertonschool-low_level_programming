3dad90e863c62ffb8b27f215212daebd49962ed7
MalikHolber <kamalatluxanov13@gmail.com>
2025-12-14 05:21:23 +0000

    a

--- pointers_arrays_strings/2-strlen.c
+++ pointers_arrays_strings/2-strlen.c
@@ -1,22 +1,16 @@
-/*
- * File: 2-strlen.c
- * Auth: Brennan D Baraban
- */
-
-#include "main.h"
-
 /**
- * _strlen - Returns the length of a string.
- * @str: The string to get the length of.
- *
- * Return: The length of @str.
+ * _strlen - returns the length of a string.
+ * @s: tested string
+ * Return: the length of the string.
  */
-size_t _strlen(const char *str)
+int _strlen(char *s)
 {
-	size_t length = 0;
+	int i;
 
-	while (*str++)
-		length++;
-
-	return (length);
+	i = 0;
+	while (*(s + i) != '\0')
+	{
+		i++;
+	}
+	return (i);
 }
